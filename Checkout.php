3122cb08a77c9d3404ead8361eda33f67354d929
Raymond Mumo <RMumo@sidianbank.co.ke>
2018-06-22 21:08:47 +0300

    Check out form

---
+++ Checkout.php
@@ -0,0 +1,18 @@
+<?php 
+?>
+
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Mpesa Checkout</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <img src= "m.jpg"/>
+    <form method="post" action="requestcheckout.php">
+</body>
+</html>
